4769e389e879d8ee33243e0a95e77032cc5d3ae8
Matthew <mcap@brandeis.edu>
2022-03-16 15:56:59 -0400

    updated category to have the month sort

--- category.py
+++ category.py
@@ -90,3 +90,12 @@ class Category():
         ''',(rowid,))
         con.commit()
         con.close()
+    
+    def month_check(self):
+        con = sqlite3.connect(self.dbfile)
+        cur = con.cursor()
+        cur.execute("SELECT * FROM categories ORDER BY date")
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return to_cat_dict(tuples)
