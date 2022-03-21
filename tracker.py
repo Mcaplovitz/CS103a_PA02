4df71ae17ad6097d30d90462fa2da328ac530eee
panydulce <64445698+panydulce@users.noreply.github.com>
2022-03-21 16:54:09 -0400

    added 7

--- tracker.py
+++ tracker.py
@@ -101,8 +101,10 @@ def process_choice(choice):
         transactions.delete(deletion)
         print("We Have Sucessfully Deleted The Transaction From The Database")
 
-    # elif choice == '7':  # summarize transactions by date
-    #Made by Matthew Caplovitz
+    elif choice == '7':  # summarize transactions by date
+        #Made By Pedro Estrada
+        date = transactions.date_sort()
+        print_transactions(date)
 
     elif choice == '8':  # summarize transactions by month
         # Made by Matthew Caplovitz
